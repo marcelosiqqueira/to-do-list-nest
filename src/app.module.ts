4c75bab2f1ad8583f9fef19e1a01a10f7a896d36
Marcelo Siqueira Filho <marcelosiqqueira@gmail.com>
2023-12-17 10:21:28 -0300

    import mongoose module and taskmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
